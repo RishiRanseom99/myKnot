3b96f1509fef4efd51386778b2404de584ba30a3
RishiRanseom99 <41407371+RishiRanseom99@users.noreply.github.com>
2020-12-05 17:32:38 +0530

    added payment and return payment files

---
+++ payment.php
@@ -0,0 +1,63 @@
+
+<?php
+
+$orderId=time();
+$orderAmount=$_POST['order__amount'];
+$customerName=$_POST['customer_name'];
+$customerEmail=$_POST['customer_email'];
+$customerPhone=$_POST['customer_phone'];
+$orderCurrency="INR";
+$orderNote=$_POST['order__note'];
+$returnUrl="http://localhost/myKnot-gh-pages/return.php";
+$notifyUrl="";
+
+$orderDetails=array();
+
+$orderDetails["orderId"]=$orderId;
+$orderDetails["orderAmount"]=$orderAmount;
+$orderDetails["customerName"]=$customerName;
+$orderDetails["customerEmail"]=$customerEmail;
+$orderDetails["customerPhone"]=$customerPhone;
+$orderDetails["orderCurrency"]=$orderCurrency;
+$orderDetails["orderNote"]=$orderNote;
+$orderDetails["returnUrl"]=$returnUrl;
+$orderDetails["notifyUrl"]=$notifyUrl;
+$orderDetails["appId"]="16659e73fe0b71d24294caa1695661";
+$orderDetails["signature"]=getSignature($orderDetails);
+
+// echo json_encode($orderDetails);
+
+function getSignature($postData)
+{
+  
+  $secretKey = "348a00ab3ee5b662f0122897e3c7bfbf4082ddf2";
+  ksort($postData);
+  $signatureData = "";
+  foreach ($postData as $key => $value){
+       $signatureData .= $key.$value;
+  }
+  $signature = hash_hmac('sha256', $signatureData, $secretKey,true);
+  $signature = base64_encode($signature);
+  return $signature;
+}
+
+
+
+?>
+ 
+<!-- //http://localhost/myKnot-gh-pages/payment.php -->
+
+<form id="redirectForm" method="post" action="https://test.cashfree.com/billpay/checkout/post/submit">
+    <input type="hidden" name="appId" value="<?php echo $orderDetails["appId"] ?>"/>
+    <input type="hidden" name="orderId" value="<?php echo $orderDetails["orderId"] ?>"/>
+    <input type="hidden" name="orderAmount" value="<?php echo $orderDetails["orderAmount"] ?>"/>
+    <input type="hidden" name="orderCurrency" value="<?php echo $orderDetails["orderCurrency"] ?>"/>
+    <input type="hidden" name="orderNote" value="<?php echo $orderDetails["orderNote"] ?>"/>
+    <input type="hidden" name="customerName" value="<?php echo $orderDetails["customerName"] ?>"/>
+    <input type="hidden" name="customerEmail" value="<?php echo $orderDetails["customerEmail"] ?>"/>
+    <input type="hidden" name="customerPhone" value="<?php echo $orderDetails["customerPhone"] ?>"/>
+    <input type="hidden" name="returnUrl" value="<?php echo $orderDetails["returnUrl"] ?>"/>
+    <input type="hidden" name="notifyUrl" value="<?php echo $orderDetails["notifyUrl"] ?>"/>
+    <input type="hidden" name="signature" value="<?php echo $orderDetails["signature"] ?>"/>
+  </form>
+  <script>document.getElementById("redirectForm").submit();</script>
---
+++ return.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php  
+ $orderId = $_POST["orderId"];
+ $orderAmount = $_POST["orderAmount"];
+ $referenceId = $_POST["referenceId"];
+ $txStatus = $_POST["txStatus"];
+ $paymentMode = $_POST["paymentMode"];
+ $txMsg = $_POST["txMsg"];
+ $txTime = $_POST["txTime"];
+ $signature = $_POST["signature"];
+ $data = $orderId.$orderAmount.$referenceId.$txStatus.$paymentMode.$txMsg.$txTime;
+ $secretkey="348a00ab3ee5b662f0122897e3c7bfbf4082ddf2";
+ $hash_hmac = hash_hmac('sha256', $data, $secretkey, true) ;
+ $computedSignature = base64_encode($hash_hmac);
+ if ($signature == $computedSignature) {
+    echo "<h1 style='text-align:center;'>Succesfully purchased, Check your mail </h1>";
+  } else {
+    echo "<h1>Unsuccesfully try again</h1>";
+ }
+ header( "refresh:5;url=http://localhost/myKnot-gh-pages/index.html#services" );
+ ?>
+ <div style="text-align:center;">
+     <h2 >Redirecting back to services in <span id="counter">5</span> second(s)</h2>
+</div>
+    <script>
+     function countdown() {
+    var i = document.getElementById('counter');
+    
+   if (parseInt(i.innerHTML)!=0) {
+    i.innerHTML = parseInt(i.innerHTML)-1;
+   }
+}
+setInterval(function(){ countdown(); },1000);
+        </script>
+</body>
+</html>
+
+
+<script type="text/javascript">
+
+</script>
